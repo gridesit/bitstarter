64a710021ec74137d5783eb7502832d054296c13
Grigorios Dedes <g.dedes@gmail.com>
2013-07-09 20:28:02 +0000

    Increase Buffer

--- web.js
+++ web.js
@@ -7,9 +7,8 @@ var buffer = new Buffer(5000);
 app.get('/', function(request, response) { 
   var temp = fs.readFileSync('index.html');
   var mystring = temp.toString();
-  //var mylength = temp.length;
   buffer.write(mystring);
-  response.send(buffer.toString("utf-8");
+  response.send(buffer.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
