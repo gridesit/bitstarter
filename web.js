9e8d669983b23a41c9aa5c95773d1f05b9fda8f3
Grigorios Dedes <g.dedes@gmail.com>
2013-07-19 22:38:44 +0000

    Improved code

--- web.js
+++ web.js
@@ -1,17 +1,15 @@
 var express = require('express');
 var fs = require('fs');
+var htmlfile = "index.html"
 
 var app = express.createServer(express.logger());
-var buffer = new Buffer(5000);
 
 app.get('/', function(request, response) { 
-  var temp = fs.readFileSync('index.html');
-  var mystring = temp.toString();
-  buffer.write(mystring);
-  response.send(buffer.toString("utf-8", 0, temp.length));
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
